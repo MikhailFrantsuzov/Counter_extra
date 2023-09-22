ca275eff7e173a1cda9c7e3495dd432fac1b5dfb
Mikhail Frantsuzov <m.francuzov@gmail.com>
2023-09-22 14:59:17 +0200

    Создан счетчик из усложненной задачи. Дополнительно, вне рамок задания, добавлена кнопка "Очистить историю" для удобства тестирования работы приложения.

--- Counter_extra/ViewController.swift
+++ Counter_extra/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    private var counterValue = 0
+    private var changeHistory = "История изменений:"
+    private var currentTime = ""
+    @IBOutlet weak private var labelText: UITextView!
+    @IBOutlet weak private var labelCounter: UILabel!
+    @IBOutlet weak private var buttonPlus: UIButton!
+    @IBOutlet weak private var buttonMinus: UIButton!
+    @IBOutlet weak private var buttonReset: UIButton!
+    @IBOutlet weak private var buttonHistoryClear: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        currentTime = dateFormatter.string(from: Date())
     }
-
-
+    @IBAction private func valuePlus(_ sender: Any) {
+        counterValue += 1
+        changeHistory.append("\n[\(currentTime)] Значение изменено на +1")
+        labelCounter.text = "\(counterValue)"
+        labelText.text = "\(changeHistory)"
+    }
+    @IBAction private func valueMinus(_ sender: Any) {
+        counterValue -= 1
+        if counterValue >= 0 {
+            labelCounter.text = "\(counterValue)"
+            changeHistory.append("\n[\(currentTime)] Значение изменено на -1")
+            labelText.text = "\(changeHistory)"
+        } else {
+            changeHistory.append("\n[\(currentTime)] Попытка уменьшить значение счётчика ниже 0")
+            labelText.text = "\(changeHistory)"
+            return counterValue = 0
+        }
+    }
+    @IBAction private func valueReset(_ sender: Any) {
+        counterValue = 0
+        labelCounter.text = "\(counterValue)"
+        changeHistory.append("\n[\(currentTime)] Значение сброшено")
+        labelText.text = "\(changeHistory)"
+    }
+    @IBAction private func historyClear(_ sender: Any) {
+        changeHistory = "История изменений:"
+        labelText.text = "\(changeHistory)"
+    }
 }
 
